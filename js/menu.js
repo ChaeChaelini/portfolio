7a5f8023784135ee7ad02395cbab6b5fdb2946bf
ChaeChaelini <142187098+ChaeChaelini@users.noreply.github.com>
2023-10-20 00:22:00 +0900

    프로젝트부분 제이쿼리수정

--- js/menu.js
+++ js/menu.js
@@ -10,7 +10,7 @@ gnb.on({
         let index = tg.index();
         let section = sections.eq(index);
         console.log(1);
-        
+
         let offset = section.offset().top;
         $('html,body').stop().animate({ scrollTop: offset }, 1000, 'easeOutCirc');
     }
@@ -45,4 +45,66 @@ win.on('scroll', function () {
     //  $('nav').removeClass('sticky');
     //}
     sct > 400 ? $('nav').addClass('sticky') : $('nav').removeClass('sticky');
+});
+let speed = Math.floor(win.height() * 0.5);
+let topArr = [];
+let winSCT;
+
+
+const project = $('.project');
+//sections.offsetTop
+project.each(function (i, o) {
+    const sectionTop = $(o).offset().top;
+    topArr.push(sectionTop)
+}) 
+win.on('scroll', () => {
+    winSCT = win.scrollTop();
+    if (winSCT > topArr[0] && winSCT < topArr[1]) {
+        project.eq(0).addClass('is-animated').siblings().removeClass('is-animated');
+        pipScroll();
+    }
+    if (winSCT > topArr[1] - speed && winSCT < topArr[2]) {
+        project.eq(1).addClass('is-animated').siblings().removeClass('is-animated');
+        pipScroll();
+    }
+    if (winSCT > topArr[2] - speed && winSCT < topArr[3]) {
+        project.eq(2).addClass('is-animated').siblings().removeClass('is-animated');
+        pipScroll();
+    }
+    if (winSCT > topArr[3] - speed) {
+        project.eq(3).addClass('is-animated').siblings().removeClass('is-animated');
+        pipScroll();   
+    }
+
+});
+
+
+function pipScroll(params) {
+    const devices = ['.mockup.pc'];
+    
+    $.each(devices, function (i, deviceEl) {
+        console.log(deviceEl);
+        const device = $(deviceEl);
+        const screen = device.find('.screen');
+        const mask = device.find('.mask');
+        const hightDifference = screen.innerHeight() - mask.innerHeight();
+        console.log(hightDifference);
+        device.on({
+            mouseenter: function () {
+                if (project.hasClass('is-animated')) {
+                    screen.stop().animate({ top: -hightDifference }, 1000);
+                }
+            },
+            mouseleave: function () {
+                if (project.hasClass('is-animated'))
+                {
+                    screen.stop().animate({ top: 0 }, 1000);
+                }
+            }
+        });
+    });
+
+}
+win.on('resize', function () {
+    pipScroll();
 });
