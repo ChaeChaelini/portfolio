947179e307994b6e8e86b172218adb21be5900bd
ChaeChaelini <https://github.com/ChaeChaelini/project1.git>
2023-10-27 18:16:36 +0900

    아트워크 구조 수정

--- js/menu.js
+++ js/menu.js
@@ -278,16 +278,42 @@ $(window).resize(function () {
 
 // popup 창 띄우기
 
-$(document).ready(function () {
-  $('.artWork1').click(function () {
-    $('.artWork1_popup').show();
+// $(document).ready(function () {
+//   $('.artWork1').click(function () {
+//     $('.artWork1_popup').show();
+
+//   });
+//   $('.close').click(function () {
+//     $('.artWork1_popup').hide();
+//   });
+// })
+
+// -----
+$(() => {
+  const tabWrapper = $('.tab-wrapper');
+
+  tabWrapper.each(function () {
+    let currentEl = $(this);
+    const targetLink = currentEl.find('.tab-menu a');
+    const tabContent = currentEl.find('.tab-content>div')
+    const close = $('.close');
+
+    targetLink.on('click', function (e) {
+      e.preventDefault();
+      let tg = $(this);
+      let currentLink = tg.attr("href");
+      tabContent.hide();
+      $(currentLink).slideDown();
+      targetLink.removeClass('active');
+      tg.addClass('active');
+
+    });
 
   });
-  $('.close').click(function () {
-    $('.artWork1_popup').hide();
-  });
-})
 
+  close.on('click', function () {
+    $('.tab-content_wrap').hide();
+  });s
 
-
+});
 
